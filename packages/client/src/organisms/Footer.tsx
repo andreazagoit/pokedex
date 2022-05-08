4bc583615b5c553c8e745f29ddeb7ed971894f3d
andrea.zago <npm>
2022-05-08 22:14:45 +0200

    feat: added footer

--- packages/client/src/organisms/Footer.tsx
+++ packages/client/src/organisms/Footer.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { Layout } from "antd";
+const { Footer: AntFooter } = Layout;
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <AntFooter style={{ textAlign: "center" }}>
+      <a
+        href="https://www.linkedin.com/in/andreazagoit/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Developed by Andrea Zago
+      </a>
+    </AntFooter>
+  );
 };
 
 export default Footer;
